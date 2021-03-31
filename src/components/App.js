350b9372093966e87810fadcee321dab09dbf8bf
Noman Rauf <nomi589@hotmail.com>
2021-03-31 12:01:39 +0300

    Change App to class component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import Nav from "./Nav";
 import TopArticles from "./TopArticles";
 import NewArticles from "./NewArticles";
 
-export default function App() {
-  return (
-    <Router>
-      <Nav />
-      <Route path="/" exact component={TopArticles} />
-      <Route path="/new" component={NewArticles} />
-    </Router>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Route path="/" exact component={TopArticles} />
+        <Route path="/new" component={NewArticles} />
+      </Router>
+    );
+  }
 }
