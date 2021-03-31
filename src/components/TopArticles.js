de9955dee80d07911a3bdbd03e68059539c79c57
Noman Rauf <nomi589@hotmail.com>
2021-03-31 15:29:06 +0300

    Add ability to show loading and errors

--- src/components/TopArticles.js
+++ src/components/TopArticles.js
@@ -1,7 +1,35 @@
 import React from "react";
+import getArticles from "../helpers/api";
 
 export default class TopArticles extends React.Component {
+  state = {
+    articles: null,
+    error: null,
+  };
+
+  componentDidMount() {
+    getArticles("top")
+      .then(() => {
+        this.setState({ articles: "success" });
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+  }
+
   render() {
-    return "TopArticles";
+    const { articles, error } = this.state;
+
+    return (
+      <React.Fragment>
+        {articles === null && error === null && "loading..."}
+        {error && (
+          <p>
+            Error in fetching articles. Please check your internet connection
+          </p>
+        )}
+        {articles && <p>{articles}</p>}
+      </React.Fragment>
+    );
   }
 }
