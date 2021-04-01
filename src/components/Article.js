f8c4a128dc49cdbe4e92670f93ab77bec85f66fd
Noman Rauf <nomi589@hotmail.com>
2021-04-01 15:25:10 +0300

    Display article id on Article page

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,7 +1,16 @@
 import React from "react";
+import queryString from "query-string";
 
 export default class Article extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      article: queryString.parse(this.props.location.search),
+    };
+  }
+
   render() {
-    return "Article";
+    return <p>{this.state.article.id}</p>;
   }
 }
