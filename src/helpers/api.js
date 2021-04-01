f6997959cb458a8f3d8b9db11cb3ee4a84b805d4
Noman Rauf <nomi589@hotmail.com>
2021-04-01 13:57:13 +0300

    Rename symbols

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,17 +1,17 @@
-export default function getHeadlines(type = "top", articleCount = 10) {
+export default function getHeadlines(type = "top", itemCount = 10) {
   const endpoint = `https://hacker-news.firebaseio.com/v0/${type}stories.json`;
 
-  return getIds(endpoint, articleCount)
-    .then((articleIds) => getArticles(articleIds))
-    .then((articles) => {
-      return articles.map((article) => {
+  return getIds(endpoint, itemCount)
+    .then((ids) => getItems(ids))
+    .then((items) => {
+      return items.map((item) => {
         return {
-          title: article.title,
-          url: article.url,
-          author: article.by,
-          time: article.time,
-          numberOfComments: article.kids ? article.kids.length : 0,
-          id: article.id,
+          title: item.title,
+          url: item.url,
+          author: item.by,
+          time: item.time,
+          numberOfComments: item.kids ? item.kids.length : 0,
+          id: item.id,
         };
       });
     })
@@ -20,28 +20,28 @@ export default function getHeadlines(type = "top", articleCount = 10) {
     });
 }
 
-function getIds(endpoint, articleCount) {
+function getIds(endpoint, itemCount) {
   return fetch(endpoint)
     .then((res) => {
       return res.json();
     })
     .then((data) => {
-      return data.splice(0, articleCount);
+      return data.splice(0, itemCount);
     })
     .catch((error) => {
       throw Error(error);
     });
 }
 
-function getArticles(articleIds) {
-  const articleByIdEndpoints = articleIds.map(
+function getItems(ids) {
+  const itemByIdEndpoints = ids.map(
     (articleId) =>
       `https://hacker-news.firebaseio.com/v0/item/${articleId}.json`
   );
 
   return Promise.allSettled(
-    articleByIdEndpoints.map((articleEndpoint) => {
-      return fetch(articleEndpoint).then((res) => res.json());
+    itemByIdEndpoints.map((itemEndpoint) => {
+      return fetch(itemEndpoint).then((res) => res.json());
     })
   ).then((results) => {
     const fulfilledResults = results.filter(
