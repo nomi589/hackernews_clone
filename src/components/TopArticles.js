6709c90a6a74ca0780817bb925f27ca9a9ae0392
Noman Rauf <nomi589@hotmail.com>
2021-04-01 10:46:19 +0300

    Display Top articles received via API

--- src/components/TopArticles.js
+++ src/components/TopArticles.js
@@ -1,6 +1,27 @@
 import React from "react";
 import getHeadlines from "../helpers/api";
 
+function Article(props) {
+  const { title, url, author, time, numberOfComments, id } = props.article;
+  /**
+   * 'time' is a Unix timestamp which is in seconds
+   * JavaScript's Date() accepts Unix timestamp but expects milliseconds
+   * Hence the multiplication by 1000
+   */
+  const dateTime = new Date(time * 1000).toLocaleString();
+
+  return (
+    <article>
+      <p>
+        <a href={url}>{title}</a>
+      </p>
+      <p>
+        by {author} on {dateTime} with {numberOfComments} comments
+      </p>
+    </article>
+  );
+}
+
 export default class TopArticles extends React.Component {
   state = {
     articles: null,
@@ -10,7 +31,7 @@ export default class TopArticles extends React.Component {
   componentDidMount() {
     getHeadlines("top")
       .then((articles) => {
-        this.setState({ articles: JSON.stringify(articles, null, 2) });
+        this.setState({ articles });
       })
       .catch((error) => {
         this.setState({ error });
@@ -24,7 +45,10 @@ export default class TopArticles extends React.Component {
       <React.Fragment>
         {articles === null && error === null && "loading..."}
         {error && <p>{error.message}</p>}
-        {articles && <p>{articles}</p>}
+        {articles &&
+          articles.map((article) => {
+            return <Article key={article.id} article={article} />;
+          })}
       </React.Fragment>
     );
   }
