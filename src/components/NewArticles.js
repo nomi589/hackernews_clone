c4d1196d1f2d5616439e1acd2bbd3267a06d182f
Noman Rauf <nomi589@hotmail.com>
2021-04-01 11:21:35 +0300

    Complete new articles display

--- src/components/NewArticles.js
+++ src/components/NewArticles.js
@@ -1,7 +1,35 @@
 import React from "react";
+import ArticleHeadline from "./ArticleHeadline";
+import getHeadlines from "../helpers/api";
 
 export default class NewArticles extends React.Component {
+  state = {
+    articles: null,
+    error: null,
+  };
+
+  componentDidMount() {
+    getHeadlines("new")
+      .then((articles) => {
+        this.setState({ articles });
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+  }
+
   render() {
-    return "NewArticles";
+    const { articles, error } = this.state;
+
+    return (
+      <React.Fragment>
+        {articles === null && error === null && "loading..."}
+        {error && <p>{error.message}</p>}
+        {articles &&
+          articles.map((article) => {
+            return <ArticleHeadline key={article.id} article={article} />;
+          })}
+      </React.Fragment>
+    );
   }
 }
